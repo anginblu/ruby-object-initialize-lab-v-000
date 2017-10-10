00b2b4b183745ddf0a6f35274455d9f38cd82d7c
Jacqueline Lee <anginblu@gmail.com>
2017-10-10 01:22:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,14 @@ class Dog
   @name = name
   @breed = breed
   end
-def name=(dog_name)
-  @name = dog_name
+def name=(name)
+  @name = name
 end
 def name
   @name
-end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
